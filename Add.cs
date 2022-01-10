defa34475cf938d463b42a33d5a9c85025731bd5
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-10 13:05:04 +0530

    [raja] added the adding of two numbers

---
+++ Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Add
+    {
+        public static void addNumbers()
+        {
+            Console.WriteLine("Enter a First Number");
+            int first=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Second Number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            int result = first + second;
+            Console.WriteLine("addition of two numbers=" +  result);
+            
+
+        }
+
+    }
+}
