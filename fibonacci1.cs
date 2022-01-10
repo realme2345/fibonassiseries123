ac26e468da4fffe6ed9ff4d25e9e3f781029202e
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-10 10:30:07 +0530

    adding fibonacci series

---
+++ fibonacci1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class fibonacci1
+    {
+        public static void ReadValuesFromUser()
+        {
+            int firstNumber = 0, SecondNumber = 1, nextNumber, numberOfElements;
+            Console.Write("Enter the number of elements to Print : ");
+            numberOfElements = int.Parse(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please Enter a number greater than two");
+            }
+            else
+            {
+                //First print first and second number
+                Console.Write(firstNumber + " " + SecondNumber + " ");
+                //Starts the loop from 2 because 0 and 1 are already printed
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber;
+                    Console.Write(nextNumber + " ");
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+        }
+    }
+}
